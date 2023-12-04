f3713eb70247f619db8322adc94f6866ae83c80d
ghadayi <ghada_y_i@hotmail.com>
2023-12-04 13:47:15 +0400

    Update pipeline.js

--- pipeline.js
+++ pipeline.js
@@ -15,7 +15,7 @@ pipeline {
         stage('Build') {
             steps {
                // bat "msbuild SampleApp.sln /p:Configuration=Release" // Build command
-                bat "${tool 'MSBuild'} SampleApp.sln /p:Configuration=Release"
+                bat "\"${tool 'MSBuild'}\" SampleApp.sln /p:Configuration=Release"
 
             }
         }
