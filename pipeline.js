23459cac3a7526e7036802d3a792d557e2e19e6e
Ghada <ghada_y_i@hotmail.com>
2023-12-04 15:48:50 +0400

    update

--- pipeline.js
+++ pipeline.js
@@ -5,6 +5,11 @@ pipeline {
         msbuild 'MSBuild' // The name of the MSBuild installation
     }
 
+    environment {
+        // Define necessary environment variables
+        DOCKER_IMAGE = 'your-docker-image-name' // Replace with your Docker image name
+        GKE_CREDENTIALS_ID = 'your-gke-credentials-id' // Jenkins credentials ID for Google Cloud
+    }
     stages {
         stage('Checkout') {
             steps {
@@ -14,28 +19,55 @@ pipeline {
 
         stage('Build') {
             steps {
-                 // Restore NuGet packages
-                 bat 'dotnet restore SampleApp.sln'
+                // Restore NuGet packages
+                bat 'dotnet restore SampleApp.sln'
 
-                 bat "\"C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\MSBuild\\Current\\Bin\\MSBuild.exe\" SampleApp.sln /p:Configuration=Release"
+                bat "\"C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\MSBuild\\Current\\Bin\\MSBuild.exe\" SampleApp.sln /p:Configuration=Release"
 
             }
         }
 
-       // stage('Test') {
+        // stage('Test') {
         //    steps {
-                // Run tests here (e.g., using NUnit or MSTest)
+        // Run tests here (e.g., using NUnit or MSTest)
         //    }
-      //  }
+        //  }
 
         stage('Dockerize') {
             steps {
                 script {
-                    docker.build("Dockerfile")
+                    // Build Docker image
+                    docker.build(env.DOCKER_IMAGE)
+                }
+            }
+        }
+        // Additional stages like Docker Push, Deployment, etc. can be added here
+        stage('Push Docker Image') {
+            steps {
+                script {
+                    // Push to Docker registry
+                    docker.withRegistry('https://registry.hub.docker.com', 'docker-credentials-id') {
+                        docker.image(env.DOCKER_IMAGE).push('latest')
+                    }
+                }
+            }
+        }
+        stage('Deploy to GKE') {
+            steps {
+                script {
+                    // Set up Google Cloud SDK
+                    sh "gcloud auth activate-service-account --key-file=${env.GKE_CREDENTIALS_ID}"
+                    sh "gcloud container clusters get-credentials your-cluster-name --zone your-cluster-zone --project your-gcp-project"
+
+                    // Update Kubernetes deployment
+                    // Assuming you have a deployment YAML file in your repository
+                    sh "kubectl apply -f k8s-deployment.yml"
+
+                    // Additional Kubernetes commands if necessary
                 }
             }
         }
 
-        // Additional stages like Docker Push, Deployment, etc. can be added here
+        // ... other stages ...
     }
 }
